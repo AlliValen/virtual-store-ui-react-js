2e1b2a2641ff2592a706a14fe4dbc6f05a8a80d9
Konny-aguero <konnyaguero@gmail.com>
2024-10-21 15:41:32 -0600

    Update Sombrillas.jsx

--- src/components/Categorías/Muebles de Patio/Subcategorias/Sombrillas Exterior/Sombrillas.jsx
+++ src/components/Categorías/Muebles de Patio/Subcategorias/Sombrillas Exterior/Sombrillas.jsx
@@ -21,18 +21,18 @@ const ProductoItem = ({ producto }) => {
 const SombrillasProducto = () => {
     // Define una lista de productos con sus propiedades id, nombre, precio e imagen.
     const productos = [
-        { id: 1, nombre: '  ', precio: 500, img: require('./img/  ') },
-        { id: 2, nombre: '  ', precio: 400, img: require('./img/  ') },
-        { id: 3, nombre: '  ', precio: 300, img: require('./img/  ') },
-        { id: 4, nombre: '  ', precio: 450, img: require('./img/  ') },
-        { id: 5, nombre: '  ', precio: 350, img: require('./img/  ') },
-        { id: 6, nombre: '  ', precio: 600, img: require('./img/  ') },
-        { id: 7, nombre: '  ', precio: 600, img: require('./img/  ') },
-        { id: 8, nombre: '  ', precio: 600, img: require('./img/  ') },
-        { id: 9, nombre: '  ', precio: 600, img: require('./img/  ') },
-        { id: 10, nombre: '  ', precio: 600, img: require('./img/  ') },
-        { id: 11, nombre: '  ', precio: 600, img: require('./img/  ') },
-        { id: 12, nombre: '  ', precio: 600, img: require('./img/  ') },
+        { id: 1, nombre: 'Sombrilla Sencilla', precio: 500, img: require('./img/sombrilla.jpg') },
+        { id: 2, nombre: 'Sombrilla Azul', precio: 400, img: require('./img/sombrilla_azul.jpg') },
+        { id: 3, nombre: 'Sombrilla Blanca', precio: 300, img: require('./img/sombrilla_blanca.jpg') },
+        { id: 4, nombre: 'Sombrilla Blanca Redonda', precio: 450, img: require('./img/sombrilla_blanca_redonda.jpg') },
+        { id: 5, nombre: 'Sombrilla con Luz', precio: 350, img: require('./img/sombrilla_con_luz.jpg') },
+        { id: 6, nombre: 'Sombrilla Cuadrada', precio: 600, img: require('./img/sombrilla_cuadrada.jpg') },
+        { id: 7, nombre: 'Sombrilla de Pared', precio: 600, img: require('./img/sombrilla_de_pared.jpg') },
+        { id: 8, nombre: 'Sombrilla Decorativa', precio: 600, img: require('./img/sombrilla_decorativa.jpg') },
+        { id: 9, nombre: 'Sombrilla Roja', precio: 600, img: require('./img/sombrilla_roja.jpg') },
+        { id: 10, nombre: 'Sombrilla Roja Redonda', precio: 600, img: require('./img/sombrilla_roja_redonda.jpg') },
+        { id: 11, nombre: 'Sombrilla Rosa', precio: 600, img: require('./img/sombrilla_rosa.jpg') },
+        { id: 12, nombre: 'Sombrilla Rosa Cuadrada', precio: 600, img: require('./img/sombrilla_rosa_cuadrada.jpg') },
 
     ];
 
