3e992387e19a47513c19bd812664bf8984f40427
joha <151862300+johitaa@users.noreply.github.com>
2024-10-21 14:23:19 -0600

    Update SillasProducto.jsx

--- src/components/Categorías/Comedores/Subcategorias/Sillas/SillasProducto.jsx
+++ src/components/Categorías/Comedores/Subcategorias/Sillas/SillasProducto.jsx
@@ -1,7 +1,23 @@
 // Importa React para definir un componente funcional.
 import React from 'react';
-// Importa el componente SillasItem que se usará para renderizar cada producto.
-import SillasItem from './SillasItem';
+import './sillas.css'; // Importa el archivo CSS correctamente
+import { FaShoppingCart } from 'react-icons/fa';
+
+
+// Componente ProductoItem
+const ProductoItem = ({ producto }) => {
+    return (
+        <div className="product-card">
+            <img src={producto.img} alt={producto.nombre} />
+            <h3>{producto.nombre}</h3>
+            <p>{producto.descripcion}</p>
+            <p>${producto.precio}</p>
+            <button className="add-to-cart-button">
+                <FaShoppingCart /> Agregar al carrito
+            </button>
+        </div>
+    );
+};
 
 // Define el componente SillasProducto que lista todos los productos de tipo Lamparas.
 const SillasProducto = () => {
@@ -24,14 +40,15 @@ const SillasProducto = () => {
     // Renderiza el componente. Muestra un título y un grid de productos que son mapeados desde el array de productos.
     return (
         <div className="comedores-productos">
-            {/* Títulos de la sección */}
-            <h1>Comedores</h1>
+            <div className="title-container"> {/* Contenedor del título */}
             <h1>Sillas</h1>
+            <div className="decorative-line"></div> {/* Línea decorativa */}
+            </div>
             {/* Renderiza la lista de productos dentro de un contenedor de grid. */}
             <div className="productos-grid">
                 {/* Recorre la lista de productos y para cada uno renderiza un componente SillasItem */}
                 {productos.map((producto) => (
-                    <SillasItem key={producto.id} producto={producto} />
+                    <ProductoItem key={producto.id} producto={producto} />
                 ))}
             </div>
         </div>
