87d3c3dd18e7c8d930a4dc2cf0dfbf137eca14db
V413-Castillo <castillovale477@gmail.com>
2024-10-21 10:20:11 -0600

    Add files via upload

--- src/components/Categorías/Comedores/Subcategorias/Juegos de comedor/JuegosComedorProductos.jsx
+++ src/components/Categorías/Comedores/Subcategorias/Juegos de comedor/JuegosComedorProductos.jsx
@@ -1,1 +1,42 @@
+// Importa React para definir un componente funcional.
+import React from 'react';
+// Importa el componente JuegosComedorItem que se usará para renderizar cada producto.
+import JuegosComedorItem from './JuegosComedorItem';
 
+// Define el componente JuegoComedorProducto que lista todos los productos de tipo Lamparas.
+const JuegoComedorProducto = () => {
+    // Define una lista de productos con sus propiedades id, nombre, precio e imagen.
+    const productos = [
+        { id: 1, nombre: 'Comedor Bancabrids', precio: 500, img: require('./img/comedor_bancabridson.jpg') },
+        { id: 2, nombre: 'Comedor Bar Bridson', precio: 300, img: require('./img/comedor_bar_bridson.jpg') },
+        { id: 3, nombre: 'Comedor Bar Odium', precio: 150, img: require('./img/comedor_bar_odium.jpg') },
+        { id: 4, nombre: 'Comedor Bar Skempton', precio: 400, img: require('./img/comedor_bar_skempton.jpg') },
+        { id: 5, nombre: 'Comedor Benox', precio: 250, img: require('./img/comedor_benox.jpg') },
+        { id: 6, nombre: 'Comedor Caitbrook', precio: 700, img: require('./img/comedor_caitbrook.jpg') },
+        { id: 7, nombre: 'Comedor Hallanden', precio: 120, img: require('./img/comedor_hallanden.jpg') },
+        { id: 8, nombre: 'Comedor Hyndell', precio: 400, img: require('./img/comedor_hyndell.jpg') },
+        { id: 9, nombre: 'Comedor Maysville', precio: 350, img: require('./img/comedor_maysville.jpg') },
+        { id: 10, nombre: 'Comedor Sanbriar', precio: 90, img: require('./img/comedor_sanbriar.jpg') },
+        { id: 11, nombre: 'Comedor Skempton', precio: 50, img: require('./img/comedor_skempton.jpg') },
+        { id: 12, nombre: 'Comedor Stonehollow', precio: 200, img: require('./img/comedor_stonehollow.jpg') },
+    ];
+
+    // Renderiza el componente. Muestra un título y un grid de productos que son mapeados desde el array de productos.
+    return (
+        <div className="comedores-productos">
+            {/* Títulos de la sección */}
+            <h1>Comedores</h1>
+            <h1>Juegos de comedor</h1>
+            {/* Renderiza la lista de productos dentro de un contenedor de grid. */}
+            <div className="productos-grid">
+                {/* Recorre la lista de productos y para cada uno renderiza un componente JuegosComedorItem */}
+                {productos.map((producto) => (
+                    <JuegosComedorItem key={producto.id} producto={producto} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+// Exporta el componente JuegoComedorProducto para que pueda ser utilizado en otras partes de la aplicación.
+export default JuegoComedorProducto;
