d4cf1e4debc4a43c4bc014c4d32e8814d7d766d1
Konny-aguero <konnyaguero@gmail.com>
2024-10-21 14:23:30 -0600

    Update and rename EspejosProducto.jsx to Espejos.jsx

--- src/components/Categorías/Accesorios/Subcategorias/Espejos/Espejos.jsx
+++ src/components/Categorías/Accesorios/Subcategorias/Espejos/Espejos.jsx
@@ -1,7 +1,21 @@
 // Importa React para definir un componente funcional.
 import React from 'react';
-// Importa el componente EspejosItem que se usará para renderizar cada producto.
-import EspejosItem from './EspejosItem';
+import './Espejos.css';
+import { FaShoppingCart } from 'react-icons/fa';
+// Componente ProductoItem
+const ProductoItem = ({ producto }) => {
+    return (
+        <div className="product-card">
+            <img src={producto.img} alt={producto.nombre} />
+            <h3>{producto.nombre}</h3>
+            <p>{producto.descripcion}</p>
+            <p>${producto.precio}</p>
+            <button className="add-to-cart-button">
+                <FaShoppingCart /> Agregar al carrito
+            </button>
+        </div>
+    );
+};
 
 // Define el componente EspejosProductos que lista todos los productos de tipo Espejos.
 const EspejosProducto = () => {
@@ -23,15 +37,16 @@ const EspejosProducto = () => {
 
     // Renderiza el componente. Muestra un título y un grid de productos que son mapeados desde el array de productos.
     return (
-        <div className="espejos-productos">
-            {/* Títulos de la sección */}
-            <h1>Accesorios</h1>
-            <h1>Espejos</h1>
-            {/* Renderiza la lista de productos dentro de un contenedor de grid. */}
+        <div className="sala-productos">
+            <div className="title-container"> {/* Contenedor del título */}
+            <h1>Productos de Accesorios</h1>
+            <div className="decorative-line"></div> {/* Línea decorativa */}
+            </div>
             <div className="productos-grid">
-                {/* Recorre la lista de productos y para cada uno renderiza un componente EspejosItem */}
                 {productos.map((producto) => (
-                    <EspejosItem key={producto.id} producto={producto} />
+                    // Mapea los productos para crear un componente ProductoItem por cada uno.
+                    // Se pasa el producto como prop y se utiliza el id como clave.
+                    <ProductoItem key={producto.id} producto={producto} />
                 ))}
             </div>
         </div>
