98367e37980cb0d8fffaca65f8bc8d138c961877
Konny-aguero <konnyaguero@gmail.com>
2024-10-22 15:28:21 -0600

    Update SillasProducto.jsx

--- src/components/Categorías/Comedores/Subcategorias/Sillas/SillasProducto.jsx
+++ src/components/Categorías/Comedores/Subcategorias/Sillas/SillasProducto.jsx
@@ -23,18 +23,18 @@ const ProductoItem = ({ producto }) => {
 const SillasProducto = () => {
     // Define una lista de productos con sus propiedades id, nombre, precio e imagen.
     const productos = [
-        { id: 1, nombre: 'Sillas Berringer', precio: 500, img: require('./img/silla_berringer.jpg') },
-        { id: 2, nombre: 'Sillas Bolanburg', precio: 300, img: require('./img/silla_bolanburg.jpg') },
-        { id: 3, nombre: 'Sillas Centiar', precio: 150, img: require('./img/silla_centiar.jpg') },
-        { id: 4, nombre: 'Sillas Glambrey', precio: 400, img: require('./img/silla_glambrey.jpg') },
-        { id: 5, nombre: 'Sillas Haddigan', precio: 250, img: require('./img/silla_haddigan.jpg') },
-        { id: 6, nombre: 'Sillas Madanere', precio: 700, img: require('./img/silla_madanere.jpg') },
-        { id: 7, nombre: 'Sillas Owingsville', precio: 120, img: require('./img/silla_owingsville.jpg') },
-        { id: 8, nombre: 'Sillas Realyn', precio: 400, img: require('./img/silla_realyn.jpg') },
-        { id: 9, nombre: 'Sillas Tripton', precio: 350, img: require('./img/silla_tripton.jpg') },
-        { id: 10, nombre: 'Sillas Tylercreek', precio: 90, img: require('./img/silla_tylercreek.jpg') },
-        { id: 11, nombre: 'Sillas Whitesburg', precio: 50, img: require('./img/silla_whitesburg.jpg') },
-        { id: 12, nombre: 'Sillas Woodanville', precio: 200, img: require('./img/silla_woodanville.jpg') },
+        { id: 1, nombre: 'Sillas Berringer', precio: 40000, img: require('./img/silla_berringer.jpg') },
+        { id: 2, nombre: 'Sillas Bolanburg', precio: 30000, img: require('./img/silla_bolanburg.jpg') },
+        { id: 3, nombre: 'Sillas Centiar', precio: 90000, img: require('./img/silla_centiar.jpg') },
+        { id: 4, nombre: 'Sillas Glambrey', precio: 40000, img: require('./img/silla_glambrey.jpg') },
+        { id: 5, nombre: 'Sillas Haddigan', precio: 25000, img: require('./img/silla_haddigan.jpg') },
+        { id: 6, nombre: 'Sillas Madanere', precio: 10000, img: require('./img/silla_madanere.jpg') },
+        { id: 7, nombre: 'Sillas Owingsville', precio: 12000, img: require('./img/silla_owingsville.jpg') },
+        { id: 8, nombre: 'Sillas Realyn', precio: 40000, img: require('./img/silla_realyn.jpg') },
+        { id: 9, nombre: 'Sillas Tripton', precio: 35000, img: require('./img/silla_tripton.jpg') },
+        { id: 10, nombre: 'Sillas Tylercreek', precio: 90000, img: require('./img/silla_tylercreek.jpg') },
+        { id: 11, nombre: 'Sillas Whitesburg', precio: 50000, img: require('./img/silla_whitesburg.jpg') },
+        { id: 12, nombre: 'Sillas Woodanville', precio: 20000, img: require('./img/silla_woodanville.jpg') },
     ];
 
     // Renderiza el componente. Muestra un título y un grid de productos que son mapeados desde el array de productos.
