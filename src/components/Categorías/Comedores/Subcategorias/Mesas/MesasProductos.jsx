1752c94f65e4e6e7af74d37e470d57dcb53ba551
Konny-aguero <konnyaguero@gmail.com>
2024-10-22 15:24:53 -0600

    Update MesasProductos.jsx

--- src/components/Categorías/Comedores/Subcategorias/Mesas/MesasProductos.jsx
+++ src/components/Categorías/Comedores/Subcategorias/Mesas/MesasProductos.jsx
@@ -22,18 +22,18 @@ const ProductoItem = ({ producto }) => {
 const MesasProducto = () => {
     // Define una lista de productos con sus propiedades id, nombre, precio e imagen.
     const productos = [
-        { id: 1, nombre: 'Mesa Bolanburg', precio: 500, img: require('./img/mesa_bolanburg.jpg') },
-        { id: 2, nombre: 'Mesa Caitbrook', precio: 300, img: require('./img/mesa_caitbrook.jpg') },
-        { id: 3, nombre: 'Mesa Extensión Haddigan', precio: 150, img: require('./img/mesa_extension_haddigan.jpg') },
-        { id: 4, nombre: 'Mesa Glambrey', precio: 400, img: require('./img/mesa_glambrey.jpg') },
-        { id: 5, nombre: 'Mesa Haddigan', precio: 250, img: require('./img/mesa_haddigan.jpg') },
-        { id: 6, nombre: 'Mesa Kavara', precio: 700, img: require('./img/mesa_kavara.jpg') },
-        { id: 7, nombre: 'Mesa Kimonte', precio: 120, img: require('./img/mesa_kimonte.jpg') },
-        { id: 8, nombre: 'Mesa Moriville', precio: 400, img: require('./img/mesa_moriville.jpg') },
-        { id: 9, nombre: 'Mesa Owingsville', precio: 350, img: require('./img/mesa_owingsville.jpg') },
-        { id: 10, nombre: 'Mesa Ralene', precio: 90, img: require('./img/mesa_ralene.jpg') },
-        { id: 11, nombre: 'Mesa Whitesburg', precio: 50, img: require('./img/mesa_whitesburg.jpg') },
-        { id: 12, nombre: 'Mesa Woodanville', precio: 200, img: require('./img/mesa_woodanville.jpg') },
+        { id: 1, nombre: 'Mesa Bolanburg', precio: 50000, img: require('./img/mesa_bolanburg.jpg') },
+        { id: 2, nombre: 'Mesa Caitbrook', precio: 100000, img: require('./img/mesa_caitbrook.jpg') },
+        { id: 3, nombre: 'Mesa Extensión Haddigan', precio: 150000, img: require('./img/mesa_extension_haddigan.jpg') },
+        { id: 4, nombre: 'Mesa Glambrey', precio: 200000, img: require('./img/mesa_glambrey.jpg') },
+        { id: 5, nombre: 'Mesa Haddigan', precio: 250000, img: require('./img/mesa_haddigan.jpg') },
+        { id: 6, nombre: 'Mesa Kavara', precio: 100000, img: require('./img/mesa_kavara.jpg') },
+        { id: 7, nombre: 'Mesa Kimonte', precio: 120000, img: require('./img/mesa_kimonte.jpg') },
+        { id: 8, nombre: 'Mesa Moriville', precio: 125000, img: require('./img/mesa_moriville.jpg') },
+        { id: 9, nombre: 'Mesa Owingsville', precio: 140000, img: require('./img/mesa_owingsville.jpg') },
+        { id: 10, nombre: 'Mesa Ralene', precio: 90000, img: require('./img/mesa_ralene.jpg') },
+        { id: 11, nombre: 'Mesa Whitesburg', precio: 110000, img: require('./img/mesa_whitesburg.jpg') },
+        { id: 12, nombre: 'Mesa Woodanville', precio: 175000, img: require('./img/mesa_woodanville.jpg') },
     ];
 
     // Renderiza el componente. Muestra un título y un grid de productos que son mapeados desde el array de productos.
